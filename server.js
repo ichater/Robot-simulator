be43040eeabbb0b7bf21cdd5bfb6e93340632c35
ichater <chaterizaak@gmail.com>
2020-04-30 15:59:02 +1000

    getting there!!

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 let x, y, facing;
 
+const compassArray = [
+  { direction: "north", message: "top", add: "y", move: -1, wall: 0 },
+  { direction: "east", message: "right", add: "x", move: 1, wall: 5 },
+  { direction: "south", message: "bottom", add: "y", y: 1, wall: 5 },
+  { direction: "west", message: "left", add: "x", move: -1, wall: 0 },
+];
+
 const report = () => {
   console.log("location: " + x + ", " + y + " facing " + facing);
 };
@@ -12,52 +19,69 @@ const move = () => {
   };
   let yInt = Number.parseInt(y);
   let xInt = Number.parseInt(x);
-  switch (facing.toLowerCase()) {
-    case "south":
-      if (yInt >= 5) {
-        message("bottom");
-        y === 5;
-      } else {
-        y = yInt += 1;
-      }
 
-      break;
-    case "west":
-      if (xInt <= 1) {
-        message("left");
-        x === 0;
-      } else {
-        x = xInt += 1;
-      }
-      break;
-    case "north":
-      if (yInt <= 1) {
-        message("top");
-        y === 1;
-      } else {
-        y = yInt -= 1;
-      }
-      break;
-    case "east":
-      if (xInt >= 5) {
-        message("right");
-        x === 5;
-      } else {
-        x = xInt += 1;
-      }
-      break;
-  }
+  compassArray.findIndex(function (i) {
+    if (i.add === "x" && x < 5) {
+      x = xInt += 1;
+    }
+    //  else {
+    //   message(i.message);
+    // }
+  });
+  compassArray.findIndex(function (i) {
+    if (i.add === "y" && y < 5) {
+      y = yInt += 1;
+    }
+    // else {
+    //   message(i.message);
+    // }
+  });
+
+  // switch (facing.toLowerCase()) {
+  //   case "south":
+  //     if (yInt >= 5) {
+  //       message("bottom");
+  //       y === 5;
+  //     } else {
+  //       y = yInt += 1;
+  //     }
+
+  //     break;
+  //   case "west":
+  //     if (xInt <= 1) {
+  //       message("left");
+  //       x === 0;
+  //     } else {
+  //       x = xInt += 1;
+  //     }
+  //     break;
+  //   case "north":
+  //     if (yInt <= 1) {
+  //       message("top");
+  //       y === 1;
+  //     } else {
+  //       y = yInt -= 1;
+  //     }
+  //     break;
+  //   case "east":
+  //     if (xInt >= 5) {
+  //       message("right");
+  //       x === 5;
+  //     } else {
+  //       x = xInt += 1;
+  //     }
+  //     break;
+  // }
 };
 
-const compassArray = ["north", "south", "east", "west"];
-
 const rightTurn = () => {
-  for (let i = compassArray.length; i >= 0; i--) {
-    if (facing === compassArray[i]) {
-      if (facing !== compassArray[compassArray.length]) {
-        facing = compassArray[i + 1];
+  for (let i = compassArray.length - 1; i >= 0; i--) {
+    if (facing === compassArray[i].direction) {
+      if (facing === compassArray[compassArray.length - 1].direction) {
+        facing = compassArray[0].direction;
+        break;
       } else {
-        facing = compassArray[0];
+        facing = compassArray[i + 1].direction;
       }
     }
   }
@@ -65,11 +89,13 @@ const rightTurn = () => {
 
 const leftTurn = () => {
   for (let i = 0; i <= compassArray.length; i++) {
-    if (facing === compassArray[i]) {
-      if (facing !== compassArray[0]) {
-        facing = compassArray[i - 1];
+    if (facing === compassArray[i].direction) {
+      if (facing !== compassArray[0].direction) {
+        facing = compassArray[i - 1].direction;
+        break;
       } else {
-        facing = compassArray[compassArray.length];
+        facing = compassArray[compassArray.length - 1].direction;
+        break;
       }
     }
   }
@@ -83,16 +109,21 @@ const chooseX = (input) => {
   x = input;
 };
 
-while (!chosenXaxis) {
-  let xAxis = prompt("Where are you on the x axis?");
+//Below is a function so it can be recalled upon the "place" command
+const placement = () => {
+  while (!chosenXaxis) {
+    let xAxis = prompt("Where are you on the x axis?");
 
-  if (xAxis >= 1 && xAxis <= 5) {
-    chooseX(xAxis);
-    chosenXaxis = true;
-  } else {
-    console.log("numbers between 1 and 5 please");
+    if (xAxis >= 1 && xAxis <= 5) {
+      chooseX(xAxis);
+      chosenXaxis = true;
+    } else {
+      console.log("numbers between 1 and 5 please");
+    }
   }
-}
+};
+
+placement();
 
 const chooseY = (input) => {
   y = input;
@@ -114,19 +145,15 @@ const chooseDirection = (input) => {
 };
 
 while (!chosenPosition) {
-  let direction = prompt("which direction are you facing?");
-
-  if (
-    direction === "north" ||
-    direction === "south" ||
-    direction === "east" ||
-    direction === "west"
-  ) {
-    chooseDirection(direction);
-    chosenPosition = true;
-  } else {
-    console.log("north, south, east or west are the only valid commands");
-  }
+  let direction = prompt(
+    "which direction are you facing? north, south,east or west?"
+  );
+  compassArray.findIndex(function (i) {
+    if (i.direction === direction.toLocaleLowerCase()) {
+      chooseDirection(direction);
+      chosenPosition = true;
+    }
+  });
 }
 
 while (chosenPosition) {
@@ -153,6 +180,7 @@ while (chosenPosition) {
       chosenPosition = false;
       chosenXaxis = false;
       chosenYAxis = false;
+      test();
       break;
     default:
       console.log(
