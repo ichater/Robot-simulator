7a960d7b02af5f7ba59e54892479afd8ec47ea58
ichater <chaterizaak@gmail.com>
2020-05-02 10:04:32 +1000

    sync

--- server.js
+++ server.js
@@ -15,25 +15,24 @@ const report = () => {
   console.log("location: " + x + ", " + y + " facing " + facing);
 };
 
-const rightTurn = () => {
-  if (facing === compassArray[compassLength - 1].direction) {
-    facing = compassArray[0].direction;
+//function below for turning either left or right can be called twice
+const turn = (x, y, z) => {
+  if (facing === compassArray[x].direction) {
+    facing = y;
   } else {
     facing =
-      compassArray[compassArray.findIndex((e) => facing === e.direction) + 1]
+      compassArray[compassArray.findIndex((e) => facing === e.direction) + z]
         .direction;
   }
 };
 
-const leftTurn = () => {
-  if (facing === compassArray[0].direction) {
-    facing = compassArray[compassLength - 1].direction;
-  } else {
-    facing =
-      compassArray[compassArray.findIndex((e) => facing === e.direction) - 1]
-        .direction;
-  }
-};
+function rightTurn() {
+  turn(compassLength - 1, compassArray[0].direction, 1);
+}
+
+function leftTurn() {
+  turn(0, compassArray[compassLength - 1].direction, -1);
+}
 
 const move = () => {
   const message = (i) => {
@@ -41,7 +40,6 @@ const move = () => {
   };
   let yInt = Number.parseInt(y);
   let xInt = Number.parseInt(x);
-
   for (let i = 0; i <= compassLength - 1; i++) {
     if (facing === compassArray[i].direction) {
       if (compassArray[i].add === "x" && xInt !== compassArray[i].wall) {
@@ -65,6 +63,9 @@ const chooseX = (input) => {
 const chooseY = (input) => {
   y = input;
 };
+const chooseDirection = (input) => {
+  facing = input;
+};
 
 const chooseAxis = (axisStr, func, chosenAxis) => {
   while (!chosenAxis) {
@@ -78,54 +79,59 @@ const chooseAxis = (axisStr, func, chosenAxis) => {
   }
 };
 
-chooseAxis("x", chooseX, chosenXaxis);
-chooseAxis("y", chooseY, chosenYAxis);
-
-const chooseDirection = (input) => {
-  facing = input;
+const chooseposition = () => {
+  while (!chosenPosition) {
+    let direction = prompt(
+      "which direction are you facing? north, south,east or west?"
+    );
+    compassArray.findIndex(function (i) {
+      if (i.direction === direction.toLocaleLowerCase()) {
+        chooseDirection(direction);
+        chosenPosition = true;
+      }
+    });
+  }
 };
 
-while (!chosenPosition) {
-  let direction = prompt(
-    "which direction are you facing? north, south,east or west?"
-  );
-  compassArray.findIndex(function (i) {
-    if (i.direction === direction.toLocaleLowerCase()) {
-      chooseDirection(direction);
-      chosenPosition = true;
+const movement = () => {
+  while (chosenPosition) {
+    let guess = prompt("What's Wall-Es next action? ");
+    switch (guess) {
+      case "move":
+        console.log("Moving forward!");
+        move();
+        break;
+      case "left":
+        console.log("turning left");
+        leftTurn();
+        break;
+      case "right":
+        console.log("turning right");
+        rightTurn();
+        break;
+      case "report":
+        console.log("report");
+        report();
+        break;
+      case "place":
+        console.log("choose placement!");
+        chosenPosition = false;
+        chosenXaxis = false;
+        chosenYAxis = false;
+        place();
+        break;
+      default:
+        console.log(
+          "COMMAND UNAVALIBLE! Avalible commands include: move, left, right. report and place. Please stick to the script"
+        );
     }
-  });
-}
-
-while (chosenPosition) {
-  let guess = prompt("What's Wall-Es next action? ");
-  switch (guess) {
-    case "move":
-      console.log("Moving forward!");
-      move();
-      break;
-    case "left":
-      console.log("turning left");
-      leftTurn();
-      break;
-    case "right":
-      console.log("turning right");
-      rightTurn();
-      break;
-    case "report":
-      console.log("report");
-      report();
-      break;
-    case "place":
-      console.log("choose placement!");
-      chosenPosition = false;
-      chosenXaxis = false;
-      chosenYAxis = false;
-      placement();
-      break;
-    default:
-      console.log(
-        "COMMAND UNAVALIBLE! Avalible commands include: move, left, right. report and place. Please stiock to the script"
-      );
   }
-}
+};
+
+const place = () => {
+  chooseAxis("x", chooseX, chosenXaxis);
+  chooseAxis("y", chooseY, chosenYAxis);
+  chooseposition();
+  movement();
+};
+place();
